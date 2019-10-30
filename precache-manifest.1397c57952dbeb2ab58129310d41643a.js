8a682db106194b865ae64d1869914596b5eebc20
Viktor2350 <ryjuk39@gmail.comm>
2019-10-30 13:14:11 +0200

    Updates

---
+++ precache-manifest.1397c57952dbeb2ab58129310d41643a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1bfa3cb3ff56d9800be7260d95428e",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "5175e6caf84f282084ab",
+    "url": "/goit-react-hw-04-reader/static/css/main.022464cc.chunk.css"
+  },
+  {
+    "revision": "8dd51d99f7a09290dd78",
+    "url": "/goit-react-hw-04-reader/static/js/2.d5c56ca6.chunk.js"
+  },
+  {
+    "revision": "5175e6caf84f282084ab",
+    "url": "/goit-react-hw-04-reader/static/js/main.7a719101.chunk.js"
+  },
+  {
+    "revision": "c8cf3110a5e60b07fce8",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.6ca2603f.js"
+  }
+]);
